1cb4f105e52489b85a26eb1bbb38abfa2156dab1
Reshid-505 <hreshid505@gmail.com>
2023-12-12 02:59:33 +0400

    last commit

--- src/services/api/BASE_URL.js
+++ src/services/api/BASE_URL.js
@@ -1,2 +1,2 @@
-// export let BASE_URL="https://65565fe884b36e3a431faa36.mockapi.io"
-export let BASE_URL="http://localhost:3000"
+export let BASE_URL="https://65565fe884b36e3a431faa36.mockapi.io"
+// export let BASE_URL="http://localhost:3000"
